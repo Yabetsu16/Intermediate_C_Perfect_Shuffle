c14fcfd1bbfbb8fccc3889c68d6205598c7f1d96
Yabetsu <it.jabez16@gmail.com>
2024-09-10 17:27:14 +0800

    Finish

---
+++ Bondoc_JabezJoshua_Intermediate_27.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char original[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char deck[27], shuffle[27];
+    const int shufsize = 13;
+    int count, s, d;
+
+    printf("Original deck: %s\n", original);
+    // Duplicate the original for shuffling
+    strcpy(deck, original);
+    count = 0;
+    while (1)
+    {
+        // s skips 2 d increments
+        for (d = 0, s = 0; d < shufsize; d++, s += 2)
+        {
+            shuffle[s] = deck[d];
+            shuffle[s + 1] = deck[d + shufsize];
+        }
+        shuffle[s] = '\0'; // cap the string
+        count++;
+        printf("%2d: %s\n", count, shuffle);
+        
+        // stop if the deck is restored;
+        if (strcmp(original, shuffle) == 0)
+            break;
+
+        // reset the deck to start over
+        strcpy(deck, shuffle);
+    }
+    printf("Deck restored after %d iterations\n", count);
+
+    return 0;
+}
